8e7267ad10d2e608ad0848ea21c9bdee5d507c5f
Corey <Corey255A1@hotmail.com>
2023-01-15 16:01:00 -0500

    Woo Working Youtube API!

--- src/components/PlaybackComponent.tsx
+++ src/components/PlaybackComponent.tsx
@@ -2,39 +2,34 @@
 // www.wundervisionenvisionthefuture.com
 import React from "react";
 import { PlaylistItem } from "../data/PlaylistItem";
-import { YoutubePlayer } from "./YoutubePlayer";
+import { MediaPlayer, YoutubePlayer } from "./YoutubePlayer";
 
 export interface PlaybackComponentProps{
     playlistItem:PlaylistItem|null;
 }
 export class PlaybackComponent extends React.Component<PlaybackComponentProps>{
-    private _controller:((msg:string)=>void) | null;
+    private _mediaPlayer:MediaPlayer | null;
     constructor(props:PlaybackComponentProps){
         super(props);
 
-        this._controller = null;
+        this._mediaPlayer = null;
     }
 
-    setController(controller:(message:string)=>void){
-        this._controller = controller;
-    }
-
-    youtubeController(msg:string){
-        if(this._controller == null) { return; }
-        this._controller(msg)
+    setMediaPlayer(mediaPlayer:MediaPlayer){
+        this._mediaPlayer = mediaPlayer;
     }
 
     render(): React.ReactNode {
         return (<div>
             <div>Currently Playing</div>
             <div>
-                <YoutubePlayer setController={this.setController.bind(this)}></YoutubePlayer>
+                <YoutubePlayer setMediaPlayer={this.setMediaPlayer.bind(this)}></YoutubePlayer>
             </div>
             <div>
-                <button>SkipBack</button>
-                <button onClick={()=>{this.youtubeController("play")}}>Play</button>
-                <button>Pause</button>
-                <button>SkipForward</button>
+                <button onClick={()=>{this._mediaPlayer?.loadVideoById("Qt0-9mO-ZXY",0)}}>SkipBack</button>
+                <button onClick={()=>{this._mediaPlayer?.play()}}>Play</button>
+                <button onClick={()=>{this._mediaPlayer?.pause()}}>Pause</button>
+                <button onClick={()=>{this._mediaPlayer?.loadVideoById("viIQYswj1Rk",0)}}>SkipForward</button>
             </div>
             <div>
             <input type="checkbox"></input>
--- src/components/YoutubePlayer.tsx
+++ src/components/YoutubePlayer.tsx
@@ -1,110 +1,120 @@
 import React, { Ref } from "react";
-declare global{
-    interface Window{
-        YT:{
-            Player:any;
+declare global {
+    interface Window {
+        YT: {
+            Player: any;
         },
-        onYouTubeIframeAPIReady:()=>void
+        onYouTubeIframeAPIReady: () => void
     }
 }
-namespace YT{
-    export interface Player{
-        playVideo:()=>void;
+namespace YT {
+    export interface Player {
+        playVideo: () => void;
     };
 }
 
-export interface YoutubePlayerProps{
-    setController:(controller:((msg:string)=>void))=>void;
+export interface MediaPlayer{
+    play:()=>void;
+    pause:()=>void;
+    loadVideoById:(videoId:string, startSeconds:number)=>void;
 }
 
-export class YoutubePlayer extends React.Component<YoutubePlayerProps>{
-    private _width:number;
-    private _height:number;
-    private _id:string;
-    private _player:YT.Player | null;
-    private _playerController:any | null
-    private static isInitializeYoutubeCalled:boolean = false;
-    private static pendingYoutubeCallbacks:Array<()=>void> = [];
+export interface YoutubePlayerProps {
+    setMediaPlayer: (controller: MediaPlayer) => void;
+}
 
-    constructor(props:YoutubePlayerProps){
-        
+export class YoutubePlayer extends React.Component<YoutubePlayerProps> implements MediaPlayer{
+    private _width: number;
+    private _height: number;
+    private _id: string;
+    private _player: YT.Player | null;
+    private _playerController: any | null
+    private static isInitializeYoutubeCalled: boolean = false;
+    private static pendingYoutubeCallbacks: Array<() => void> = [];
+
+    constructor(props: YoutubePlayerProps) {
+
         super(props);
         this._width = 320;
         this._height = 320;
         this._id = "youtube-test";
         this._player = null;
         this._playerController = null;
-        this.props.setController(this.controller.bind(this));
-        console.log("Construct");
+        this.props.setMediaPlayer(this);
+
+
     }
 
-    private static youTubeIframeAPIReady(){
-        YoutubePlayer.pendingYoutubeCallbacks.forEach((callback)=>{
+    private static youTubeIframeAPIReady() {
+        YoutubePlayer.pendingYoutubeCallbacks.forEach((callback) => {
             callback();
         })
         YoutubePlayer.pendingYoutubeCallbacks = [];
     }
 
-    private static initializeYoutube(apiReadyCallback:()=>void){
-        if(!YoutubePlayer.isInitializeYoutubeCalled){
+    private static initializeYoutube(apiReadyCallback: () => void) {
+        if (!YoutubePlayer.isInitializeYoutubeCalled) {
             YoutubePlayer.isInitializeYoutubeCalled = true;
             const tag = document.createElement('script');
             tag.src = 'https://www.youtube.com/iframe_api';
-        
+
             // onYouTubeIframeAPIReady will load the video after the script is loaded
             window.onYouTubeIframeAPIReady = YoutubePlayer.youTubeIframeAPIReady;
-        
+
             const firstScriptTag = document.getElementsByTagName('script')[0];
             firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
         }
         YoutubePlayer.pendingYoutubeCallbacks.push(apiReadyCallback);
     }
-
-    private controller(message:string){
-        console.log(message);
-        this._playerController?.playVideo();
-    }
-
-    onPlayerReady(event:any){
+    private onPlayerReady(event: any) {
         console.log("READY!");
         console.log(event);
         this._playerController = event.target;
     }
-    onPlayerStateChange(event:any){
+    private onPlayerStateChange(event: any) {
         console.log("STATE");
         console.log(event);
     }
-    loadVideo(){
+    private loadVideo() {
         console.log("Load");
         this._player = new window.YT.Player(this._id, {
             events: {
-              'playerVars':{
-                'mute':1,
-                'autoplay':1
-              },
-              'onReady': this.onPlayerReady.bind(this),
-              'onStateChange': this.onPlayerStateChange.bind(this)
+                'onReady': this.onPlayerReady.bind(this),
+                'onStateChange': this.onPlayerStateChange.bind(this)
             }
         });
     }
-    componentDidMount(){
+
+    public play(){
+        this._playerController?.playVideo();
+    }
+
+    public pause(){
+        this._playerController?.pauseVideo();
+    }
+
+    public loadVideoById(id:string, startSeconds:number){
+        this._playerController?.loadVideoById(id, startSeconds);
+    }
+
+    componentDidMount() {
         console.log(this._id);
-        if(window.YT === undefined){
+        if (window.YT === undefined) {
             YoutubePlayer.initializeYoutube(this.loadVideo.bind(this));
-        }else{
+        } else {
             this.loadVideo();
         }
     }
 
     render(): React.ReactNode {
-        console.log("REnder?")
+        console.log("Render?")
         return (
-            <iframe 
-                width={this._width} 
-                height={this._height} 
-                src="http://www.youtube.com/embed/B-w86z_gbP8?enablejsapi=1" 
-                title="YouTube video player" 
-                frameBorder="0" 
+            <iframe
+                width={this._width}
+                height={this._height}
+                src="http://www.youtube.com/embed/B-w86z_gbP8?enablejsapi=1&mute=1"
+                title="YouTube video player"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
                 id={this._id}
